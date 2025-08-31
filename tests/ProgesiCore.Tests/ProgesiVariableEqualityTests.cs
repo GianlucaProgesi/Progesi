af0f9dbb907f0aaf3afcd5c3e16558b37d72c638
gianlucamarchese <gianluca.marchese79@gmail.com>
2025-08-31 09:04:59 +0100

    tests: dedupe hash tests; fix metadata id>0; make hash value-sensitive

---
+++ tests/ProgesiCore.Tests/ProgesiVariableEqualityTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using ProgesiCore;
+using Xunit;
+
+namespace ProgesiCore.Tests
+{
+    public class ProgesiVariableEqualityTests
+    {
+        [Fact]
+        public void Equality_Ignores_DependsFrom_Order()
+        {
+            var v1 = new ProgesiVariable(id: 1, name: "A", value: null, dependsFrom: new[] { 3, 1, 2 }, metadataId: 7);
+            var v2 = new ProgesiVariable(id: 1, name: "A", value: null, dependsFrom: new[] { 2, 3, 1 }, metadataId: 7);
+
+            v1.Should().Be(v2);
+
+            var set = new HashSet<ProgesiVariable> { v1, v2 };
+            set.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void Inequality_When_Value_Differs()
+        {
+            var a = new ProgesiVariable(1, "K", 42, new[] { 1, 2, 3 }, metadataId: 7);
+            var b = new ProgesiVariable(1, "K", "42", new[] { 1, 2, 3 }, metadataId: 7);
+
+            a.Should().NotBe(b);
+        }
+    }
+}
---
+++ tests/ProgesiCore.Tests/ValueObjectEqualityTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using ProgesiCore;
+using Xunit;
+
+namespace ProgesiCore.Tests
+{
+    // VO minimale per esercitare la semantica del base ValueObject
+    public sealed class DummyVo : ValueObject
+    {
+        public int A { get; }
+        public string B { get; }
+
+        public DummyVo(int a, string b)
+        {
+            A = a;
+            B = b;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return A;
+            yield return B ?? string.Empty;
+        }
+    }
+
+    public class ValueObjectEqualityTests
+    {
+        [Fact]
+        public void Equals_And_Hash_Work()
+        {
+            var x = new DummyVo(1, "k");
+            var y = new DummyVo(1, "k");
+            var z = new DummyVo(2, "k");
+
+            x.Equals(y).Should().BeTrue();
+            x.GetHashCode().Should().Be(y.GetHashCode());
+            x.Equals(z).Should().BeFalse();
+
+            var set = new HashSet<DummyVo> { x, y, z };
+            set.Should().HaveCount(2); // x e y si deduplicano
+        }
+    }
+}
