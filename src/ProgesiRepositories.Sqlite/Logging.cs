ed133a2632d08302585ac69fd18c1c19efeaa1c4
gianlucamarchese <gianluca.marchese79@gmail.com>
2025-09-03 20:55:11 +0100

    sqlite: track Logging.cs (override .gitignore)

---
+++ src/ProgesiRepositories.Sqlite/Logging.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProgesiRepositories.Sqlite
+{
+    public interface IProgesiLogger
+    {
+        void Debug(string message);
+        void Info(string message);
+        void Warn(string message);
+        void Error(string message);
+        void Error(Exception ex, string message);
+    }
+
+    public sealed class NullLogger : IProgesiLogger
+    {
+        public static readonly NullLogger Instance = new NullLogger();
+        private NullLogger() { }
+
+        public void Debug(string message) { }
+        public void Info(string message) { }
+        public void Warn(string message) { }
+        public void Error(string message) { }
+        public void Error(Exception ex, string message) { }
+    }
+}
